c31835461d9f4510a01a486d9de74a2367118e17
rotamitanian <tamitania12@gmail.com>
2021-10-25 16:17:22 +0700

    Commit initiation

---
+++ app/src/main/java/com/rotamitanian/higherorlower/MainActivity.java
@@ -0,0 +1,43 @@
+package com.rotamitanian.higherorlower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int randomNumber;
+
+    public void makeToast(String string){
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+    }
+
+    public void guess(View view){
+        EditText guessEditText = (EditText) findViewById(R.id.editTextNumber);
+
+        int guessInt = Integer.parseInt(guessEditText.getText().toString());
+
+        if(guessInt > randomNumber){
+            makeToast("Lower!!!");
+        }else if(guessInt < randomNumber){
+            makeToast("Higher!!!");
+        }else{
+            makeToast("That's Right! Try Again!");
+            Random rand = new Random();
+            randomNumber = rand.nextInt(20)+1;
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+    }
+}
